f61adb1f797a822df6aed4a01e05e6befacb3a94
rivkab123 <rivka.bloch@mail.huji.ac.il>
2025-09-01 15:41:35 +0300

    jackTokenizer implementation

--- 10/JackTokenizer.py
+++ 10/JackTokenizer.py
@@ -6,6 +6,8 @@ as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
 Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
 import typing
+from collections import deque
+import re
 
 
 class JackTokenizer:
@@ -92,99 +94,176 @@ class JackTokenizer:
     Note that ^, # correspond to shiftleft and shiftright, respectively.
     """
 
-    def __init__(self, input_stream: typing.TextIO) -> None:
-        """Opens the input stream and gets ready to tokenize it.
+    symbols =  {
+        '{', '}', '(', ')', '[', ']', '.',
+        ',', ';', '+', '-', '*', '/', '&',
+        '|', '<', '>', '=', '~', '^', '#' }
+
+    keywords = {
+        'class': 'CLASS',
+        'constructor': 'CONSTRUCTOR',
+        'function': 'FUNCTION',
+        'method': 'METHOD',
+        'field': 'FIELD',
+        'static': 'STATIC',
+        'var': 'VAR',
+        'int': 'INT',
+        'char': 'CHAR',
+        'boolean': 'BOOLEAN',
+        'void': 'VOID',
+        'true': 'TRUE',
+        'false': 'FALSE',
+        'null': 'NULL',
+        'this': 'THIS',
+        'let': 'LET',
+        'do': 'DO',
+        'if': 'IF',
+        'else': 'ELSE',
+        'while': 'WHILE',
+        'return': 'RETURN'
+    }
+
+
+    
+    """Opens the input stream and gets ready to tokenize it.
 
         Args:
             input_stream (typing.TextIO): input stream.
-        """
-        # Your code goes here!
-        # A good place to start is to read all the lines of the input:
-        # input_lines = input_stream.read().splitlines()
-        pass
+    """
+    def __init__(self, input_stream: typing.TextIO) -> None:
+        
+        self.tokens: deque[str] = deque()
+        self.current_token: str | None = None
+        sentences_list: list[str] = []
+        inside_block_comment = False
 
+        for line in input_stream.read().splitlines():
+            line = line.strip()
+
+            if inside_block_comment:
+                if "*/" in line:
+                    inside_block_comment = False 
+                continue
+
+            if line.startswith("/*") or line.startswith("/**"):
+                if not "*/" in line: 
+                    inside_block_comment = True
+                continue
+
+            clean_line = line.split("//")[0].strip()
+            if clean_line:
+                sentences_list.append(clean_line)
+        
+        symbols_regex = "[" + "".join(re.escape(sym) for sym in self.symbols) + "]"
+        token_pattern = re.compile( r"\"[^\n\"]*\"|[A-Za-z_][A-Za-z0-9_]*|\d+|" + symbols_regex)
+        
+        for sentence in sentences_list:
+            self.tokens.extend(token_pattern.findall(sentence))
+
+        
+    """
+    Do we have more tokens in the input?
+
+    Returns:
+        bool: True if there are more tokens, False otherwise.
+    """
     def has_more_tokens(self) -> bool:
-        """Do we have more tokens in the input?
+        
+        if self.tokens:
+            return True
+        return False
 
-        Returns:
-            bool: True if there are more tokens, False otherwise.
-        """
-        # Your code goes here!
-        pass
-
+    """
+    Gets the next token from the input and makes it the current token. 
+    This method should be called if has_more_tokens() is true. 
+    Initially there is no current token.
+    """
     def advance(self) -> None:
-        """Gets the next token from the input and makes it the current token. 
-        This method should be called if has_more_tokens() is true. 
-        Initially there is no current token.
-        """
-        # Your code goes here!
-        pass
+        
+        self.current_token = self.tokens.popleft()
 
+    """
+    Returns:
+        str: the type of the current token, can be
+        "KEYWORD", "SYMBOL", "IDENTIFIER", "INT_CONST", "STRING_CONST"
+    """
     def token_type(self) -> str:
-        """
-        Returns:
-            str: the type of the current token, can be
-            "KEYWORD", "SYMBOL", "IDENTIFIER", "INT_CONST", "STRING_CONST"
-        """
-        # Your code goes here!
-        pass
+        
+        if self.current_token in self.keywords:
+            return "KEYWORD"
+        
+        elif self.current_token in self.symbols:
+            return "SYMBOL"
 
+        elif self.current_token.isdigit():
+            value = int(self.current_token)
+            if not (0 <= value <= 32767):
+                raise ValueError (f"Integer constant out of range: {self.current_token} (must be 0–32767)")
+            else:
+                return "INT_CONST"
+        
+        elif self.current_token.startswith('"') and self.current_token.endswith('"'):
+            return "STRING_CONST"
+
+        else:
+            return "IDENTIFIER"
+
+
+    """
+    Returns:
+        str: the keyword which is the current token.
+        Should be called only when token_type() is "KEYWORD".
+        Can return "CLASS", "METHOD", "FUNCTION", "CONSTRUCTOR", "INT", 
+        "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO", 
+        "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS"
+    """
     def keyword(self) -> str:
-        """
-        Returns:
-            str: the keyword which is the current token.
-            Should be called only when token_type() is "KEYWORD".
-            Can return "CLASS", "METHOD", "FUNCTION", "CONSTRUCTOR", "INT", 
-            "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO", 
-            "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS"
-        """
-        # Your code goes here!
-        pass
+        return self.keywords[self.current_token]
 
+    
+    """
+    Returns:
+        str: the character which is the current token.
+        Should be called only when token_type() is "SYMBOL".
+        Recall that symbol was defined in the grammar like so:
+        symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
+            '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
+    """
     def symbol(self) -> str:
-        """
-        Returns:
-            str: the character which is the current token.
-            Should be called only when token_type() is "SYMBOL".
-            Recall that symbol was defined in the grammar like so:
-            symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
-              '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
-        """
-        # Your code goes here!
-        pass
-
+        return self.current_token
+        
+    
+    """
+    Returns:
+        str: the identifier which is the current token.
+        Should be called only when token_type() is "IDENTIFIER".
+        Recall that identifiers were defined in the grammar like so:
+        identifier: A sequence of letters, digits, and underscore ('_') not 
+                starting with a digit. You can assume keywords cannot be
+                identifiers, so 'self' cannot be an identifier, etc'.
+    """
     def identifier(self) -> str:
-        """
-        Returns:
-            str: the identifier which is the current token.
-            Should be called only when token_type() is "IDENTIFIER".
-            Recall that identifiers were defined in the grammar like so:
-            identifier: A sequence of letters, digits, and underscore ('_') not 
-                  starting with a digit. You can assume keywords cannot be
-                  identifiers, so 'self' cannot be an identifier, etc'.
-        """
-        # Your code goes here!
-        pass
+        return self.current_token
 
+    
+    """
+    Returns:
+        str: the integer value of the current token.
+        Should be called only when token_type() is "INT_CONST".
+        Recall that integerConstant was defined in the grammar like so:
+        integerConstant: A decimal number in the range 0-32767.
+    """
     def int_val(self) -> int:
-        """
-        Returns:
-            str: the integer value of the current token.
-            Should be called only when token_type() is "INT_CONST".
-            Recall that integerConstant was defined in the grammar like so:
-            integerConstant: A decimal number in the range 0-32767.
-        """
-        # Your code goes here!
-        pass
+        return int(self.current_token)
 
+    
+    """
+    Returns:
+        str: the string value of the current token, without the double 
+        quotes. Should be called only when token_type() is "STRING_CONST".
+        Recall that StringConstant was defined in the grammar like so:
+        StringConstant: '"' A sequence of Unicode characters not including 
+                    double quote or newline '"'
+    """
     def string_val(self) -> str:
-        """
-        Returns:
-            str: the string value of the current token, without the double 
-            quotes. Should be called only when token_type() is "STRING_CONST".
-            Recall that StringConstant was defined in the grammar like so:
-            StringConstant: '"' A sequence of Unicode characters not including 
-                      double quote or newline '"'
-        """
-        # Your code goes here!
-        pass
+        return self.current_token[1:-1]
